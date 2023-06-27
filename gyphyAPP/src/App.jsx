955cb0d67c8ebb1ee07b92ee8e111bd3c077a7b9
[Orlando Lechuga] <[orlando_lechuga125@hotmail.com]>
2023-06-27 11:50:16 -0600

    un_commit

--- gyphyAPP/src/App.jsx
+++ gyphyAPP/src/App.jsx
@@ -1,9 +1,28 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 
 function App () {
+  const [gifs, setGifs] = useState([]) // aqui se guardan las Gifs
+  const APIKEY = 'X3AMixwUoMnUHf0z1JZhllNjimNZ91aa'
+
+  // Las llamadas a la API se hacen en useEffect
+
+  useEffect(() => {
+    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${APIKEY}&limit=5&offset=0&rating=g&bundle=messaging_non_clips`)
+      .then((response) => {
+        return response.json()
+      }).then((results) => {
+        console.log(results.data)
+        return results.data()
+      }).catch((error) => {
+        console.error(error)
+      })
+  // Las comillas invertidas `` son para meter pedazos de codigo de javascript a un string. haciendo que el string tenga una parte dinámica
+  }, [])
+
   return (
     <>
-
+      <h1>Giphy App</h1>
     </>
   )
 }
