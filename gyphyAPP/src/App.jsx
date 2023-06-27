76e30f7f1c80cf81cb9f9008d0d429be4f38d07d
[Orlando Lechuga] <[orlando_lechuga125@hotmail.com]>
2023-06-27 12:43:24 -0600

    app_gif

--- gyphyAPP/src/App.jsx
+++ gyphyAPP/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import { useState, useEffect } from 'react'
+import Imagecat from './components/Imagecat'
+import { Searchbar } from './components/Searchbar'
 
 function App () {
   const [gifs, setGifs] = useState([]) // aqui se guardan las Gifs
@@ -8,21 +10,44 @@ function App () {
   // Las llamadas a la API se hacen en useEffect
 
   useEffect(() => {
-    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${APIKEY}&limit=5&offset=0&rating=g&bundle=messaging_non_clips`)
+    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${APIKEY}&limit=20&offset=0&rating=g&bundle=messaging_non_clips`)
       .then((response) => {
         return response.json()
       }).then((results) => {
-        console.log(results.data)
-        return results.data()
+        setGifs(results.data)
       }).catch((error) => {
         console.error(error)
       })
   // Las comillas invertidas `` son para meter pedazos de codigo de javascript a un string. haciendo que el string tenga una parte dinámica
   }, [])
 
+  const sendSearch = (search) => {
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${search}&limit=20&offset=0&rating=g&lang=es&bundle=messaging_non_clips`)
+      .then((response) => {
+        return response.json()
+      }).then((results) => {
+        setGifs(results.data)
+      }).catch((error) => {
+        console.error(error)
+      })
+  }
+
   return (
     <>
-      <h1>Giphy App</h1>
+      <div className='App'>
+        <Searchbar handleSearch={sendSearch} />
+        <div className='grid-cards'>
+          {
+            gifs.map((gif) => (
+              <Imagecat
+                key={gif.id}
+                title={gif.title}
+                url={gif.images.fixed_height.url}
+              />
+            ))
+          }
+        </div>
+      </div>
     </>
   )
 }
